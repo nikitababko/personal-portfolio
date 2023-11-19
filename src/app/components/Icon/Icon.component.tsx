476bc485966d1f17a9599614b1b66986a69ce1bd
Nikita Babko <niki.babko@gmail.com>
2023-11-19 21:45:48 +0700

    chore: add icon component

---
+++ src/app/components/Icon/Icon.component.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import type { IconNSTypes } from './Icon.types';
+import { useController } from './Icon.controller';
+
+export const Icon: React.FC<IconNSTypes.Props> = ({
+  color = 'black',
+  name,
+  size = 24,
+}) => {
+  const { icons } = useController(color);
+
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {icons[name](color)}
+    </svg>
+  );
+};
---
+++ src/app/components/Icon/Icon.types.ts
@@ -0,0 +1,23 @@
+import type * as CSS from 'csstype';
+import type React from 'react';
+
+export namespace IconNSTypes {
+  export type Color = CSS.Property.Color;
+
+  export type Name = 'download' | 'mouse' | 'arrowDown';
+
+  export type Props = {
+    color?: Color;
+    size?: number;
+    name: Name;
+  };
+
+  export type UseController = (color: Color) => {
+    icons: Icons;
+  };
+
+  export type Icons = Record<
+    Name,
+    (color: Color) => React.ReactNode
+  >;
+}
