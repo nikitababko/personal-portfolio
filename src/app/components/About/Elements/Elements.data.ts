ce7da34e3f26dda2cd13ecab2254fb3f81597cb5
Nikita Babko <niki.babko@gmail.com>
2023-12-06 22:00:39 +0700

    refactor: add global constant `ASSETS_PATH`

--- src/app/components/About/Elements/Elements.data.ts
+++ src/app/components/About/Elements/Elements.data.ts
@@ -1,27 +1,28 @@
-const assetsPath = '/assets/about';
+import type { ElementsNSTypes } from './Elements.types';
+import { ASSETS_PATH } from '../../../constants';
 
-export const elements = [
+export const elements: ElementsNSTypes.Element[] = [
   {
     id: 1,
-    src: `${assetsPath}/responsive-design.png`,
+    src: `${ASSETS_PATH}/about/responsive-design.png`,
     text: `My layouts will work on any device, big or small`,
     alt: 'Responsive design icon',
   },
   {
     id: 2,
-    src: `${assetsPath}/fast.png`,
+    src: `${ASSETS_PATH}/about/fast.png`,
     text: `Fast load times and lag free interaction, my highest priority`,
     alt: 'Performance icon',
   },
   {
     id: 3,
-    src: `${assetsPath}/light-bulb.png`,
+    src: `${ASSETS_PATH}/about/light-bulb.png`,
     text: `Strong preference for easy to use, intuitive UX/UI`,
     alt: 'Light bulb icon',
   },
   {
     id: 4,
-    src: `${assetsPath}/success.png`,
+    src: `${ASSETS_PATH}/about/success.png`,
     text: `Websites don't have to be static, I love making pages come to life`,
     alt: 'Success icon',
   },
